ba96d4685b8e162fad5dc04b2b0db6a11d426f45
Kit Rodolfa <shaycrk@gmail.com>
2025-06-08 20:56:48 -0700

    update CM updated date

--- cm_finder/rallyes/lick-observatory-cms.js
+++ cm_finder/rallyes/lick-observatory-cms.js
@@ -500,5 +500,5 @@ var prev_loc = {coords: {latitude: 37.373407, longitude:  -122.082302}};
 var prev_hdng = 5;
 
 window.onload = function() {
-  document.getElementById("cms-ver").innerHTML = "CMs Updated 2025-05-21";
+  document.getElementById("cms-ver").innerHTML = "CMs Updated 2025-06-08";
 }
